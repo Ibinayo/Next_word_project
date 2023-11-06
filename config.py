c0e0e3551138d3cff1bc37af3306ccd55353a2f3
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-11-06 09:56:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,5 @@ class Config:
     """Config class for Flask app"""
     SECRET_KEY = 'Just-a-random-basic-word'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
-        # 'sqlite:///' + os.path.join(basedir, 'app.db')
-    # SQLALCHEMY_DATABASE_URI = 'postgres://next_db_62g4_user:jgaxnYDnk85xS1IOJXV7sOBpHvDxsTXP@dpg-ckv6p73amefc73dath10-a.oregon-postgres.render.com/next_db_62g4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
